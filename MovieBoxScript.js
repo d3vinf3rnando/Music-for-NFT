56e728974b1d692f4a4d59a21d216eb61bf0e498
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-06-03 20:25:50 +0530

    Add files via upload

---
+++ MovieBoxScript.js
@@ -0,0 +1,13 @@
+function clickme(){
+    document.getElementById("name").innerHTML="My name is devin";
+}
+
+function moviechange(){
+    document.getElementById("movie-name").style.display="none";
+}
+
+
+function show(){
+    document.getElementById("movie-name").style.display="block";
+}
+
